f8f31c6a8a1c3902b0fd4aae4ea3fa2958d63167
Peter Wildberger <peter@bleachr.co>
2020-03-13 22:34:02 -0500

    card

--- src/client/UserCard.tsx
+++ src/client/UserCard.tsx
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom';
 
 export const UserCard = props => {
   return (
-    <div className="col-3 user">
+    <div className="col-3 user flex-center">
+      <div className="user-card flex-center">
       <Link to={`/details/${props.login.uuid}`} style={{ color: 'inherit', textDecoration: 'inherit' }}>
-        <img src={props.picture.medium} alt="" />
+        <div className="profile-pic">
+          <img src={props.picture.medium} alt={props.name.last} />
+        </div>
         <h2>
           {props.name.first} {props.name.last}
         </h2>
         <p>{props.dob.age}</p>
       </Link>
+      </div>
     </div>
   );
 };
